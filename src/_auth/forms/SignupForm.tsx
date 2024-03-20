44b0792059ff154530ae3111458ed8bc0b831290
tien_dat_202241 <datpham@posco.net>
2024-03-20 14:47:48 +0700

    setup project

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const SignupForm = () => {
+  return (
+    <Button>Click me</Button>
+  )
+}
+
+export default SignupForm
